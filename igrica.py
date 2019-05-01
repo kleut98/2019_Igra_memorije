fad37cf169c08d293075a79d79ce8cfabebd2a37
anjavelickovic <28anja@live.com>
2019-05-01 10:12:06 +0200

    #--KADA UDJEMO U 1vs1 i brzo kliknemo na 3 kartice--treba vrv uraditi neki disable dugmica

--- igrica.py
+++ igrica.py
@@ -1,11 +1,18 @@
 from tkinter import *
 import tkinter as tk
-from tkinter import ttk # treba nam za lepsi izgled, trenutno koristimo samo kod *** da bi dugmici uvek bili iste velicine pri promeni igraca(PLAVI ZUTI)
+from tkinter import ttk # treba nam za lepsi izgled, trenutno koristimo samo kod ***da bi dugmici uvek bili iste velicine pri promeni igraca(PLAVI ZUTI)
 import os #**
 import random
 import time
 from tkinter.font import Font
 from pathlib import Path #treba nam za putanje za ucitavanje slika, kao i **
+from tkinter import messagebox
+
+#--KADA UDJEMO U 1vs1 i brzo kliknemo na 3 kartice--treba vrv uraditi neki disable dugmica
+#Treba dodati da izabrane kartice stv budu kartice (a ne bele slike)
+#8x8 i 16x16- nemamo dovoljno kartica
+#dodati mozda u menubar nesto kao zapocni novu igru?
+
 
 #globalne promenljive
 count=0
@@ -22,7 +29,7 @@ done = 0
 
 class MatrixOfButtons:
     #master je root
-    def __init__(self,master,frame,n,f_end,var1):
+    def __init__(self,master,frame,n,f_start,var1):
         #self=root a frame=f_one_player
         #valjda je master root?
 
@@ -34,9 +41,10 @@ class MatrixOfButtons:
 
         self.b=[[0 for x in range(0,n)] for x in range(0,n)] #matrica dugmica
         #****
-        self.lbl = tk.Label(frame,text="Igra ZUTI igrac") #prvi na potezu je ZUTI igrac
-        self.points_yellow = tk.Label(frame,text = "0") #labela za bodove ZUTOG igraca
-        self.points_blue = tk.Label(frame,text = "0") #labela za bodove PLAVOG igraca
+        myFont = Font(family="Segoe Print", size=10)
+        self.lbl = tk.Label(frame,text="\tIgra  ZUTI igrac",font=myFont,background='LightSteelBlue2') #prvi na potezu je ZUTI igrac
+        self.points_yellow = tk.Label(frame,text = "Zuti: 0",background='LightSteelBlue2',font=myFont) #labela za bodove ZUTOG igraca
+        self.points_blue = tk.Label(frame,text = "Plavi: 0",background='LightSteelBlue2',font=myFont) #labela za bodove PLAVOG igraca
 
         photo_matrix=[[0 for x in range(0,n)] for x in range(0,n)] #matrica slika
 
@@ -66,16 +74,16 @@ class MatrixOfButtons:
             for j in range(0,n):
                 path = os.path.join(Path().absolute(),"slike3/blank.gif")
                 photo=PhotoImage(file=path)#slika kartica
-                self.b[i][j] = ttk.Button(frame,image=photo,command=lambda x1=i, y1=j,n=n,matrica=photo_matrix,frame=frame,card_photo=photo,f_end=f_end,var1=var1: self.funkcija(x1,y1,n,matrica,frame,card_photo,f_end,var1)) #***
+                self.b[i][j] = ttk.Button(frame,image=photo,command=lambda x1=i, y1=j,n=n,matrica=photo_matrix,frame=frame,card_photo=photo,f_start=f_start,var1=var1: self.funkcija(x1,y1,n,matrica,frame,card_photo,f_start,var1)) #***
                 self.b[i][j].image=photo #*
                 self.b[i][j].grid(row = i,column = j,sticky='nsew')  
         #stavljamo na frame labele koje smo gore definisali ****
-        self.lbl.grid(row = (n+3),column = 0,sticky='nsew')
-        self.points_yellow.grid(row = (n+4),column = 0,sticky='nsew')
-        self.points_blue.grid(row = (n+5),column = 0,sticky='nsew')
+        self.lbl.grid(row = 0,column = (n+3),sticky='nsew')
+        self.points_yellow.grid(row = 1,column = (n+3),sticky='nsew')
+        self.points_blue.grid(row = 1,column = (n+4),sticky='nsew')
         
                    
-    def funkcija(self,i,j,n,photo_matrix,frame,card_photo,f_end,var1):
+    def funkcija(self,i,j,n,photo_matrix,frame,card_photo,f_start,var1):
         global pom_i,pom_j
         global player
         global points_b,points_y
@@ -107,7 +115,6 @@ class MatrixOfButtons:
             frame.after(500, lambda: open_card(self,i2,j2,photo_matrix))
             nonmatched.append(pair1) #vracamo pair1 u nonmatched listu
     
-
         def change(photo,i,j):
             global pom_i,pom_j
             self.b[i][j].config(image=photo)
@@ -125,7 +132,7 @@ class MatrixOfButtons:
             self.b[i2][j2].config(image=photo)
             self.b[i2][j2].image=photo
         def change_of_player(self,boja):
-            self.lbl.configure(text= "Igra " + boja + " igrac!")
+            self.lbl.configure(text= "\tIgra " + boja + " igrac!")
 
         def computer_playing(self,frame,photo_matrix,photo_second,card_photo):
             global nonmatched
@@ -150,65 +157,63 @@ class MatrixOfButtons:
             else:
                 player = 1
                 frame.after(800, lambda: change_random_cards(card_photo,pair1,pair2))
-                
-                frame.after(700, lambda: change_of_player(self,"ZUTI"))
+                frame.after(700, lambda: change_of_player(self,"ZUTI "))
 
         def winner(points_b,points_y):
-            myFont = Font(family="Segoe Print", size=14)
-            if points_y > points_b: 
-                lbly = tk.Label(f_end,text="Pobednik je ZUTI!",font=myFont)
-                lbly.pack()
+            def popup(text): #novi prozor u kom pitamo da li igrac zeli ponovo da igra ili da izadje iz igrice
+                msg=messagebox.askyesno("Qustion",text)
+                if msg==True:
+                    frame.destroy()
+                    raise_frame(f_start)
+                else:
+                    exit()
+            if points_y > points_b:
+                popup("Pobedio je ZUTI Igrac!\nIgraj ponovo?")
             elif points_y < points_b:
-                lblb = tk.Label(f_end,text="Pobednik je PLAVI!",font=myFont)
-                lblb.pack()
+                popup("Pobedio je PLAVI Igrac!\nIgraj ponovo?")
             else:
-                lble = tk.Label(f_end,text="Nereseno je!",font=myFont)
-                lble.pack()
-            raise_frame(f_end)
+                popup("Nereseno\nIgraj ponovo?")
 
         # zapravo program, posle gomile fja :D
-        if (pom_i!=i or pom_j!=j) and ((i,j) in nonmatched): #da ne bi nastao problem ako neko klikne dva puta na isto dugme i ne mozemo ponovo otvarati osvojene kartice
-        
+        if (pom_i!=i or pom_j!=j) and ((i,j) in nonmatched): #da ne bi nastao problem ako neko klikne dva puta na isto dugmei ne mozemo ponovo otvarati osvojene kartice
+
             global count
             count += 1
         
             open_card(self,i,j,photo_matrix)
          
-            #proveravamo da li su otvorene 2 kartice, ako jesu radimo sledece:
-            if count % 2 == 0:
-                if photo_matrix[i][j]==photo_matrix[pom_i][pom_j]:
-                    if player == 1:
+            if count % 2 == 0: #proveravamo da li su otvorene 2 kartice, ako jesu radimo sledece
+                if photo_matrix[i][j]==photo_matrix[pom_i][pom_j]: #ako su uparene
+                    if player == 1: #ako je igrao zuti igrac
                         nonmatched.remove((i,j))
                         nonmatched.remove((pom_i,pom_j))
                         points_y += 1
-                        self.points_yellow.configure(text = str(points_y))
-                        frame.after(700, lambda: change(photo_first,i,j))
+                        self.points_yellow.configure(text ="Zuti: "+ str(points_y))
+                        frame.after(700, lambda: change(photo_first,i,j)) #postavljamo na zute slicice jer su uparene
                     else:
+                        #analogno za plavog
                         nonmatched.remove((i,j))
                         nonmatched.remove((pom_i,pom_j))
                         points_b += 1
-                        self.points_blue.configure(text = str(points_b))
+                        self.points_blue.configure(text ="Plavi: "+ str(points_b))
                         frame.after(700, lambda: change(photo_second,i,j))
-                #sklanjaju se slike
                 else:
-                    frame.after(700, lambda: change(card_photo,i,j))
+                    frame.after(700, lambda: change(card_photo,i,j)) #vracamo na zatvorene slicice (jer nisu uparene dobro)
                     if player == 1:
-                        player = 2
+                        player = 2 #promenimo ko je na redu da igra
                         frame.after(700, lambda: change_of_player(self,"PLAVI"))
                         
-                        if var1 ==1: # u ovo ulazi samo ako smo uzeli opciju 1 vs rac
+                        if var1 ==1: # u ovo ulazi samo ako smo uzeli opciju 1 vs rac (onda nam je racunar ustvari plavi igrac)
                             frame.after(750, lambda: computer_playing(self,frame,photo_matrix,photo_second,card_photo))
-
                     else:
                         player = 1
-                        frame.after(500, lambda: change_of_player(self,"ZUTI"))
+                        frame.after(500, lambda: change_of_player(self,"ZUTI "))
             # ako nisu, to znaci da imamo jednu otvorenu i zelimo da zapamtimo njenu poziciju
             else:
                 pom_i=i
                 pom_j=j  
-
         
-        if points_b + points_y == n*n/2 and done ==0: #ako je done=1 onda smo presli na f_end u fji computer_playing
+        if points_b + points_y == n*n/2 and done==0: #ako je done=1 onda smo presli na f_end u fji computer_playing
             frame.after(1200,lambda : winner(points_b,points_y))
         
         
@@ -219,51 +224,67 @@ def sel(f_start):
     
     if var1.get()==1:
         print("1 vs rac")
-        f_start.destroy()
         raise_frame(f_one_player)
-        m = MatrixOfButtons(root,f_one_player,var3.get(),f_end,var1.get())
+        m = MatrixOfButtons(root,f_one_player,var3.get(),f_start,var1.get())
     else:
         print("1 vs 1")
         raise_frame(f_two_players)
-        m = MatrixOfButtons(root,f_two_players,var3.get(),f_end,var1.get())
+        m = MatrixOfButtons(root,f_two_players,var3.get(),f_start,var1.get())
 
 class MainGUI:
 
     def __init__(self, master,photo1,photo2,photo3,var1,var2,var3):
-        self.f_start = Frame(master)
+        self.f_start = Frame(master,background='LightSteelBlue2',padx=60,pady=60)
         self.f_start.grid(column=1, row=1)
-        #cemu ovo sluzi??
-        self.f_start.columnconfigure(1, weight=1)
-        self.f_start.rowconfigure(1, weight=1)
-        myFont = Font(family="Segoe Print", size=14)
-        tk.Label(self.f_start, text='Izaberite opciju:', font=myFont).grid(column = 1,row =1)    
-        tk.Radiobutton(self.f_start, text="1 vs rac", variable=var1, value=1,indicatoron=0).grid(column = 1,row =2)
-        tk.Radiobutton(self.f_start, text="1 vs 1", variable=var1, value=2,indicatoron=0).grid(column =2,row =2)
-        
-        tk.Label(self.f_start, text='Izaberite kartice:',font=myFont).grid(column = 1,row =3)
-        tk.Radiobutton(self.f_start,image = photo1, text="opcija 1", variable=var2, value=1,indicatoron = 0).grid(column =1,row =4)
-        tk.Radiobutton(self.f_start,image = photo2, text="opcija 2", variable=var2, value=2,indicatoron = 0).grid(column =2,row =4)
-        tk.Radiobutton(self.f_start,image = photo3 ,text="opcija 3", variable=var2, value=3,indicatoron = 0).grid(column =3,row =4)
 
-        tk.Label(self.f_start, text='Koliko kartica zelite?',font=myFont).grid(column = 1,row =5)
+        myFont = Font(family="Segoe Print", size=14)
+        myFontBtn = Font(family="Segoe Print", size=11)
+        
+        label=Label(self.f_start, text='Izaberite opciju:', font=myFont,background='LightSkyBlue2').grid(column = 1,row =1)    
+        rb1=tk.Radiobutton(self.f_start, text="1 vs rac", variable=var1, value=1,indicatoron=0,cursor='heart',bg='LightCyan2',fg='navy',font=myFontBtn).grid(column = 1,row =2)
+        rb2=tk.Radiobutton(self.f_start, text="1 vs 1", variable=var1, value=2,indicatoron=0,cursor='heart',bg='LightCyan2',fg='navy',font=myFontBtn).grid(column =2,row =2)
+        
+        Label(self.f_start, text='Izaberite kartice:',font=myFont,background='LightSkyBlue2').grid(column = 1,row =3)
+        tk.Radiobutton(self.f_start,image = photo, text="opcija 1", variable=var2, value=1,indicatoron = 0,cursor='heart').grid(column =1,row =4)
+        tk.Radiobutton(self.f_start,image = photo2, text="opcija 2", variable=var2, value=2,indicatoron = 0,cursor='heart').grid(column =2,row =4)
+        tk.Radiobutton(self.f_start,image = photo3 ,text="opcija 3", variable=var2, value=3,indicatoron = 0,cursor='heart').grid(column =3,row =4,padx=50)
+
+        Label(self.f_start, text='Koliko kartica zelite?',font=myFont,background='LightSkyBlue2').grid(column = 1,row =5)
         #prosledjujemo pocetni frejm (f_start)
-        tk.Radiobutton(self.f_start, text="4x4", variable=var3, value=4,command=lambda frame=self.f_start: sel(frame),indicatoron = 0).grid(column =1,row =6)
-        tk.Radiobutton(self.f_start, text="8x8", variable=var3, value=8,command=lambda frame=self.f_start: sel(frame),indicatoron = 0).grid(column =2,row =6)
-        tk.Radiobutton(self.f_start, text="16x16", variable=var3, value=16,command=lambda frame=self.f_start: sel(frame),indicatoron = 0).grid(column =3,row =6)
+        tk.Radiobutton(self.f_start, text="4x4", variable=var3, value=4,cursor='heart',bg='LightCyan2',fg='navy',font=myFontBtn,command=lambda frame=self.f_start: sel(frame),indicatoron = 0).grid(column =1,row =6)
+        tk.Radiobutton(self.f_start, text="8x8", variable=var3, value=8,cursor='heart',bg='LightCyan2',fg='navy',font=myFontBtn,command=lambda frame=self.f_start: sel(frame),indicatoron = 0).grid(column =2,row =6)
+        tk.Radiobutton(self.f_start, text="16x16", variable=var3, value=16,cursor='heart',bg='LightCyan2',fg='navy',font=myFontBtn,command=lambda frame=self.f_start: sel(frame),indicatoron = 0).grid(column =3,row =6)
         
         raise_frame(self.f_start)
 
+
 root = Tk()
 root.title('Memory game')
-#root.geometry('800x600') # Size 200, 200
+root.configure(background='LightSteelBlue2',cursor='rtl_logo')
 
-f_one_player = Frame(root)
-f_two_players = Frame(root)
-f_end = Frame(root)
+root.geometry('800x500') # Size 800,500
 
+#MENUBAR
+menubar = Menu(root)
+root.config(menu=menubar)
+fileMenu = Menu(menubar)
+helpMenu=Menu(menubar)
+
+def ispisi():
+    text="Ova igrica na na na"
+    msg=messagebox.showinfo("O igrici",text)    
+
+helpMenu.add_command(label="Pravila igre",command=ispisi)
+fileMenu.add_command(label="Izadji",command=exit)
+menubar.add_cascade(label="File", menu=fileMenu)
+menubar.add_cascade(label="Help", menu=helpMenu)
+
+
+f_one_player = Frame(root,background='LightSteelBlue2')
+f_two_players = Frame(root,background='LightSteelBlue2')
 f_one_player.grid(row=1, column=1,sticky="nsew",padx=20,pady=20)
 f_two_players.grid(row=1, column=1,sticky="nsew",padx=20,pady=20)
-f_end.grid(row=1, column=1,sticky="nsew",padx=20,pady=20)
+
 path1 = os.path.join(Path().absolute(),"slike3/1.gif")
 path2= os.path.join(Path().absolute(),"slike3/2.gif")
 path3 = os.path.join(Path().absolute(),"slike3/3.gif")
