17ba3c862fbd9c3160511d45c07e59a85e6cff79
anjavelickovic <28anja@live.com>
2019-05-01 19:51:54 +0200

    ...

--- igrica.py
+++ igrica.py
@@ -32,7 +32,7 @@ class MatrixOfButtons:
     def __init__(self,master,frame,n,f_start,var1):
         #self=root a frame=f_one_player
         #valjda je master root?
-
+        
         #formiramo listu nonmatched:
         global nonmatched
         for i in range(0,n):
@@ -46,7 +46,7 @@ class MatrixOfButtons:
         self.points_yellow = tk.Label(frame,text = "Zuti: 0",background='LightSteelBlue2',font=myFont) #labela za bodove ZUTOG igraca
         self.points_blue = tk.Label(frame,text = "Plavi: 0",background='LightSteelBlue2',font=myFont) #labela za bodove PLAVOG igraca
 
-        photo_matrix=[[0 for x in range(0,n)] for x in range(0,n)] #matrica slika
+        self.photo_matrix=[[0 for x in range(0,n)] for x in range(0,n)] #matrica slika
 
         photo_list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24] #broj slika
         list_pair=[] #lista parova=koordinate u matrici (i,j)
@@ -54,7 +54,7 @@ class MatrixOfButtons:
         for i in range(0,n):
             for j in range(0,n):
                 list_pair.append((i,j))
-                photo_matrix[i][j]=" " 
+                self.photo_matrix[i][j]=" " 
 
         while list_pair!=[]:
             broj_slike=random.choice(photo_list)
@@ -67,14 +67,15 @@ class MatrixOfButtons:
 
             #na poziciji (i,j) cuvamo naziv slike
             path = os.path.join(Path().absolute(),"slike3/"+str(broj_slike)+".gif")
-            photo_matrix[pair[0]][pair[1]]=path
-            photo_matrix[pair1[0]][pair1[1]]=path
+            self.photo_matrix[pair[0]][pair[1]]=path
+            self.photo_matrix[pair1[0]][pair1[1]]=path
             
         for i in range(0,n):
             for j in range(0,n):
                 path = os.path.join(Path().absolute(),"slike3/blank.gif")
                 photo=PhotoImage(file=path)#slika kartica
-                self.b[i][j] = ttk.Button(frame,image=photo,command=lambda x1=i, y1=j,n=n,matrica=photo_matrix,frame=frame,card_photo=photo,f_start=f_start,var1=var1: self.funkcija(x1,y1,n,matrica,frame,card_photo,f_start,var1)) #***
+                self.b[i][j] = ttk.Button(frame,image=photo,command=lambda x1=i, y1=j,n=n,frame=frame,card_photo=photo,f_start=
+                                          f_start,var1=var1: self.funkcija(x1,y1,n,frame,card_photo,f_start,var1)) #***
                 self.b[i][j].image=photo #*
                 self.b[i][j].grid(row = i,column = j,sticky='nsew')  
         #stavljamo na frame labele koje smo gore definisali ****
@@ -83,7 +84,7 @@ class MatrixOfButtons:
         self.points_blue.grid(row = 1,column = (n+4),sticky='nsew')
         
                    
-    def funkcija(self,i,j,n,photo_matrix,frame,card_photo,f_start,var1):
+    def funkcija(self,i,j,n,frame,card_photo,f_start,var1):
         global pom_i,pom_j
         global player
         global points_b,points_y
@@ -94,25 +95,25 @@ class MatrixOfButtons:
         photo_first = PhotoImage(file=pathf)
         photo_second = PhotoImage(file=paths)
 
-        def open_card(self,i,j,photo_matrix): #prikazuje sta se nalazi na kartici
-            photo=PhotoImage(file=photo_matrix[i][j])
+        def open_card(self,i,j): #prikazuje sta se nalazi na kartici
+            photo=PhotoImage(file=self.photo_matrix[i][j])
             self.b[i][j].config(image=photo)
             #moramo ponovo da postavimo sliku zbog sakupljaca otpadaka, jer ga on pokupi u suprotnom*
             self.b[i][j].image=photo
 
-        def open_random_cards(self,photo_matrix): #prikazuje random izabrane kartice (2 kartice)
+        def open_random_cards(self): #prikazuje random izabrane kartice (2 kartice)
             global nonmatched
             global pair1,pair2
             
             #biramo 2 para iz liste neosvojenih(=nonmatched) kartica
             pair1 = random.choice(nonmatched)
             (i1,j1) = pair1
-            open_card(self,i1,j1,photo_matrix)
+            open_card(self,i1,j1)
             nonmatched.remove(pair1) #sklonili smo ovaj pair1 da bi nam pair2 bio razliciti od pair1, posle cemo ga vratiti
 
             pair2 = random.choice(nonmatched)
             (i2,j2) = pair2
-            frame.after(500, lambda: open_card(self,i2,j2,photo_matrix))
+            frame.after(500, lambda: open_card(self,i2,j2))
             nonmatched.append(pair1) #vracamo pair1 u nonmatched listu
     
         def change(photo,i,j):
@@ -134,37 +135,70 @@ class MatrixOfButtons:
         def change_of_player(self,boja):
             self.lbl.configure(text= "\tIgra " + boja + " igrac!")
 
-        def computer_playing(self,frame,photo_matrix,photo_second,card_photo):
+        def computer_playing(self,frame,photo_second,card_photo):
             global nonmatched
             global player
             global points_b
             global pair1,pair2
             global done #sluzi nam da ne ispisemo 2x ko je pobednik na f_end
+            disable_buttons(self,n)
+                    
             if len(nonmatched) is 0 : 
                 frame.after(1200,lambda : winner(points_b,points_y))
                 done = 1
                 return False
-            open_random_cards(self,photo_matrix)
+            open_random_cards(self)
             (i1,j1) = pair1
             (i2,j2) = pair2
-            if photo_matrix[i1][j1]==photo_matrix[i2][j2]:
+            if self.photo_matrix[i1][j1]==self.photo_matrix[i2][j2]:
                 nonmatched.remove(pair1)
                 nonmatched.remove(pair2)
                 points_b += 1
                 self.points_blue.configure(text = str(points_b))
                 frame.after(800,lambda: change_random_cards(photo_second,pair1,pair2))
-                frame.after(1200,lambda: computer_playing(self,frame,photo_matrix,photo_second,card_photo))
+                frame.after(1200,lambda: computer_playing(self,frame,photo_second,card_photo))
             else:
                 player = 1
-                frame.after(800, lambda: change_random_cards(card_photo,pair1,pair2))
+                frame.after(900, lambda: enable_buttons(self,n))                 
+                frame.after(900, lambda: change_random_cards(card_photo,pair1,pair2))
                 frame.after(700, lambda: change_of_player(self,"ZUTI "))
+            
 
+        def defaults(): #vracamo sve globalne promenljiive na njihov default
+            global count
+            global pom_i,pom_j
+            global player
+            global points_b,points_y
+            global nonmatched
+            global pair1,pair2
+            global done
+            count=0
+            pom_i=-1
+            pom_j=-1
+            player = 1 #redni broj igraca 
+            points_b = 0 #poeni PLAVOG igraca
+            points_y = 0 #poeni ZUTOG igraca
+            nonmatched = [] #lista kartica koje nisu match-ovane ili osvojene
+            #sledeca dva para koristimo kod 1 vs rac
+            pair1 = (-1,-1) 
+            pair2 = (-1,-1)
+            done = 0
+            
+        def disable_buttons(self,n):
+            for i in range(0,n):
+                for j in range(0,n):
+                    self.b[i][j].configure(state=DISABLED)
+        def enable_buttons(self,n):
+            for i in range(0,n):
+                for j in range(0,n):
+                    self.b[i][j].configure(state=NORMAL)
+            
         def winner(points_b,points_y):
             def popup(text): #novi prozor u kom pitamo da li igrac zeli ponovo da igra ili da izadje iz igrice
                 msg=messagebox.askyesno("Qustion",text)
                 if msg==True:
-                    frame.destroy()
-                    raise_frame(f_start)
+                    defaults()
+                    MainGUI(root,photo,photo2,photo3,var1,var2,var3)
                 else:
                     exit()
             if points_y > points_b:
@@ -180,10 +214,10 @@ class MatrixOfButtons:
             global count
             count += 1
         
-            open_card(self,i,j,photo_matrix)
+            open_card(self,i,j)
          
             if count % 2 == 0: #proveravamo da li su otvorene 2 kartice, ako jesu radimo sledece
-                if photo_matrix[i][j]==photo_matrix[pom_i][pom_j]: #ako su uparene
+                if self.photo_matrix[i][j]==self.photo_matrix[pom_i][pom_j]: #ako su uparene
                     if player == 1: #ako je igrao zuti igrac
                         nonmatched.remove((i,j))
                         nonmatched.remove((pom_i,pom_j))
@@ -199,15 +233,18 @@ class MatrixOfButtons:
                         frame.after(700, lambda: change(photo_second,i,j))
                 else:
                     frame.after(700, lambda: change(card_photo,i,j)) #vracamo na zatvorene slicice (jer nisu uparene dobro)
+                    disable_buttons(self,n)   
                     if player == 1:
                         player = 2 #promenimo ko je na redu da igra
                         frame.after(700, lambda: change_of_player(self,"PLAVI"))
-                        
                         if var1 ==1: # u ovo ulazi samo ako smo uzeli opciju 1 vs rac (onda nam je racunar ustvari plavi igrac)
-                            frame.after(750, lambda: computer_playing(self,frame,photo_matrix,photo_second,card_photo))
+                            frame.after(750, lambda: computer_playing(self,frame,photo_second,card_photo))
+                        frame.after(700, lambda: enable_buttons(self,n))                 
                     else:
                         player = 1
                         frame.after(500, lambda: change_of_player(self,"ZUTI "))
+                        frame.after(700, lambda: enable_buttons(self,n))
+                        
             # ako nisu, to znaci da imamo jednu otvorenu i zelimo da zapamtimo njenu poziciju
             else:
                 pom_i=i
@@ -220,8 +257,7 @@ class MatrixOfButtons:
 def raise_frame(frame):
     frame.tkraise()
 
-def sel(f_start):
-    
+def sel(f_start):    
     if var1.get()==1:
         print("1 vs rac")
         raise_frame(f_one_player)
@@ -232,7 +268,6 @@ def sel(f_start):
         m = MatrixOfButtons(root,f_two_players,var3.get(),f_start,var1.get())
 
 class MainGUI:
-
     def __init__(self, master,photo1,photo2,photo3,var1,var2,var3):
         self.f_start = Frame(master,background='LightSteelBlue2',padx=60,pady=60)
         self.f_start.grid(column=1, row=1)
